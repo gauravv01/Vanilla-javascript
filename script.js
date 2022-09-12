23897c7144c781a9a8e7187fb3cc244b7d6b2c42
gauravv01 <vermagauravverma841@gmail.com>
2022-09-12 14:28:17 +0530

    Full-Calculator

--- script.js
+++ script.js
@@ -85,10 +85,9 @@ class Calculator {
   }
 }
 
-
-const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
+const numberButtons = document.querySelectorAll('[data-number]')
 const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
